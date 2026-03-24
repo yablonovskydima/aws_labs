0ee2af4e05632fda286840173cb25560555ac9c6
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2026-03-25 00:08:30 +0200

    lab2 added

--- main.tf
+++ main.tf
@@ -1,27 +1,40 @@
 provider "aws" {
-  region     = "eu-north-1"
+  region = "eu-north-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_iam_user" "user1" {
-  name = "az104-01a-u1"
+resource "aws_iam_group" "helpdesk" {
+  name = "helpdesk"
 }
 
-resource "aws_iam_user" "user2" {
-  name = "az104-01a-u2"
+resource "aws_iam_group_policy_attachment" "vm_contributor" {
+  group = aws_iam_group.helpdesk.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_group" "lab_group" {
-  name = "az104-01a-g1"
+resource "aws_iam_policy" "custom_support" {
+  name = "CustomSupportRequest"
+  description = "A custom contributor role for support requests."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["support:*"]
+        Resource = "*"
+      },
+      {
+        Effect = "Deny"
+        Action = ["support:AddCommunicationToCase"]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_group_membership" "add_users" {
-  name = "lab-membership"
-  group = aws_iam_group.lab_group.name
-
-  users = [
-    aws_iam_user.user1.name,
-    aws_iam_user.user2.name,
-  ]
+resource "aws_iam_group_policy_attachment" "custom_support_attach" {
+  group = aws_iam_group.helpdesk.name
+  policy_arn = aws_iam_policy.custom_support.arn
 }
