948909996fc1da19b39d52b666ea3022472baef6
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2026-03-25 00:26:00 +0200

    lab3 added

--- main.tf
+++ main.tf
@@ -1,40 +1,25 @@
 provider "aws" {
-  region = "eu-north-1"
+  region     = "eu-north-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-resource "aws_iam_group" "helpdesk" {
-  name = "helpdesk"
+resource "aws_ebs_volume" "disk1" {
+  availability_zone = "eu-north-1a"
+  size              = 32
+  type              = "standard"
+
+  tags = {
+    Name = "az104-disk1"
+  }
 }
 
-resource "aws_iam_group_policy_attachment" "vm_contributor" {
-  group = aws_iam_group.helpdesk.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
+resource "aws_ebs_volume" "disk2" {
+  availability_zone = "eu-north-1a"
+  size              = 32
+  type              = "standard"
 
-resource "aws_iam_policy" "custom_support" {
-  name = "CustomSupportRequest"
-  description = "A custom contributor role for support requests."
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = ["support:*"]
-        Resource = "*"
-      },
-      {
-        Effect = "Deny"
-        Action = ["support:AddCommunicationToCase"]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_group_policy_attachment" "custom_support_attach" {
-  group = aws_iam_group.helpdesk.name
-  policy_arn = aws_iam_policy.custom_support.arn
+  tags = {
+    Name = "az104-disk2"
+  }
 }
