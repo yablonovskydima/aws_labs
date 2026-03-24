165d93b2a4468887ed35c6d72da73216681fc155
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2026-03-24 23:28:12 +0200

    lab1 added

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_iam_user" "user1" {
+  name = "az104-01a-u1"
+}
+
+resource "aws_iam_user" "user2" {
+  name = "az104-01a-u2"
+}
+
+resource "aws_iam_group" "lab_group" {
+  name = "az104-01a-g1"
+}
+
+resource "aws_iam_group_membership" "add_users" {
+  name = "lab-membership"
+  group = aws_iam_group.lab_group.name
+
+  users = [
+    aws_iam_user.user1.name,
+    aws_iam_user.user2.name,
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
