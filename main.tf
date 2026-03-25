ec81ff29c2adf8a5b1229bda530b08409e391c3b
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2026-03-25 23:55:43 +0200

    lab4 added

--- main.tf
+++ main.tf
@@ -4,22 +4,99 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_ebs_volume" "disk1" {
-  availability_zone = "eu-north-1a"
-  size              = 32
-  type              = "standard"
+resource "aws_vpc" "core_services" {
+  cidr_block = "10.20.0.0/16"
+  tags = { Name = "CoreServicesVnet" }
+}
 
-  tags = {
-    Name = "az104-disk1"
+resource "aws_subnet" "shared_services" {
+  vpc_id            = aws_vpc.core_services.id
+  cidr_block        = "10.20.10.0/24"
+  availability_zone = "eu-north-1a"
+  tags = { Name = "SharedServicesSubnet" }
+}
+
+resource "aws_subnet" "database" {
+  vpc_id            = aws_vpc.core_services.id
+  cidr_block        = "10.20.20.0/24"
+  availability_zone = "eu-north-1a"
+  tags = { Name = "DatabaseSubnet" }
+}
+
+resource "aws_vpc" "manufacturing" {
+  cidr_block = "10.30.0.0/16"
+  tags = { Name = "ManufacturingVnet" }
+}
+
+resource "aws_subnet" "sensor1" {
+  vpc_id            = aws_vpc.manufacturing.id
+  cidr_block        = "10.30.20.0/24"
+  availability_zone = "eu-north-1a"
+  tags = { Name = "SensorSubnet1" }
+}
+
+resource "aws_subnet" "sensor2" {
+  vpc_id            = aws_vpc.manufacturing.id
+  cidr_block        = "10.30.21.0/24"
+  availability_zone = "eu-north-1a"
+  tags = { Name = "SensorSubnet2" }
+}
+
+resource "aws_security_group" "nsg_secure" {
+  name   = "myNSGSecure"
+  vpc_id = aws_vpc.core_services.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "AllowASG HTTP"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "AllowASG HTTPS"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8"]
+    description = "DenyInternetOutbound"
+  }
+
+  tags = { Name = "myNSGSecure" }
+}
+
+resource "aws_route53_zone" "public" {
+  name = "contoso.com"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.public.zone_id
+  name    = "www.contoso.com"
+  type    = "A"
+  ttl     = 1
+  records = ["10.1.1.4"]
+}
+
+resource "aws_route53_zone" "private" {
+  name = "private.contoso.com"
+
+  vpc {
+    vpc_id = aws_vpc.manufacturing.id
   }
 }
 
-resource "aws_ebs_volume" "disk2" {
-  availability_zone = "eu-north-1a"
-  size              = 32
-  type              = "standard"
-
-  tags = {
-    Name = "az104-disk2"
-  }
+resource "aws_route53_record" "sensorvm" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "sensorvm.private.contoso.com"
+  type    = "A"
+  ttl     = 1
+  records = ["10.1.1.4"]
 }
