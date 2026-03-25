f43ba388fb8ebdb1c8b18842ead15f67b0d407a2
DmytroYablonovskyi <yablonovskydmutro1@gmail.com>
2026-03-26 00:35:48 +0200

    added lab5

--- main.tf
+++ main.tf
@@ -4,99 +4,60 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_vpc" "core_services" {
-  cidr_block = "10.20.0.0/16"
-  tags = { Name = "CoreServicesVnet" }
+resource "aws_vpc" "vpc_core" {
+  cidr_block           = "10.50.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = "az104-05-vpc-core" }
 }
 
-resource "aws_subnet" "shared_services" {
-  vpc_id            = aws_vpc.core_services.id
-  cidr_block        = "10.20.10.0/24"
+resource "aws_subnet" "core" {
+  vpc_id            = aws_vpc.vpc_core.id
+  cidr_block        = "10.50.0.0/24"
   availability_zone = "eu-north-1a"
-  tags = { Name = "SharedServicesSubnet" }
+  tags = { Name = "Core" }
 }
 
-resource "aws_subnet" "database" {
-  vpc_id            = aws_vpc.core_services.id
-  cidr_block        = "10.20.20.0/24"
+resource "aws_subnet" "perimeter" {
+  vpc_id            = aws_vpc.vpc_core.id
+  cidr_block        = "10.50.1.0/24"
   availability_zone = "eu-north-1a"
-  tags = { Name = "DatabaseSubnet" }
+  tags = { Name = "perimeter" }
 }
 
-resource "aws_vpc" "manufacturing" {
-  cidr_block = "10.30.0.0/16"
-  tags = { Name = "ManufacturingVnet" }
+resource "aws_vpc" "vpc_manufacturing" {
+  cidr_block           = "10.60.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = { Name = "az104-05-vpc-manufacturing" }
 }
 
-resource "aws_subnet" "sensor1" {
-  vpc_id            = aws_vpc.manufacturing.id
-  cidr_block        = "10.30.20.0/24"
+resource "aws_subnet" "manufacturing" {
+  vpc_id            = aws_vpc.vpc_manufacturing.id
+  cidr_block        = "10.60.0.0/24"
   availability_zone = "eu-north-1a"
-  tags = { Name = "SensorSubnet1" }
+  tags = { Name = "Manufacturing" }
 }
 
-resource "aws_subnet" "sensor2" {
-  vpc_id            = aws_vpc.manufacturing.id
-  cidr_block        = "10.30.21.0/24"
-  availability_zone = "eu-north-1a"
-  tags = { Name = "SensorSubnet2" }
+resource "aws_vpc_peering_connection" "core_to_mfg" {
+  vpc_id      = aws_vpc.vpc_core.id
+  peer_vpc_id = aws_vpc.vpc_manufacturing.id
+  auto_accept = true
+  tags = { Name = "az104-05-peering" }
 }
 
-resource "aws_security_group" "nsg_secure" {
-  name   = "myNSGSecure"
-  vpc_id = aws_vpc.core_services.id
+resource "aws_route_table" "core_services" {
+  vpc_id = aws_vpc.vpc_core.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "AllowASG HTTP"
+  route {
+    cidr_block                = aws_vpc.vpc_manufacturing.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.core_to_mfg.id
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "AllowASG HTTPS"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8"]
-    description = "DenyInternetOutbound"
-  }
-
-  tags = { Name = "myNSGSecure" }
+  tags = { Name = "rt-CoreServices" }
 }
 
-resource "aws_route53_zone" "public" {
-  name = "contoso.com"
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.public.zone_id
-  name    = "www.contoso.com"
-  type    = "A"
-  ttl     = 1
-  records = ["10.1.1.4"]
-}
-
-resource "aws_route53_zone" "private" {
-  name = "private.contoso.com"
-
-  vpc {
-    vpc_id = aws_vpc.manufacturing.id
-  }
-}
-
-resource "aws_route53_record" "sensorvm" {
-  zone_id = aws_route53_zone.private.zone_id
-  name    = "sensorvm.private.contoso.com"
-  type    = "A"
-  ttl     = 1
-  records = ["10.1.1.4"]
+resource "aws_route_table_association" "core" {
+  subnet_id      = aws_subnet.core.id
+  route_table_id = aws_route_table.core_services.id
 }
