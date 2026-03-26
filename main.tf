95094fdb5cbfaef49dbb005216d82b0cb5fbd547
yablonovskydima <157278934+yablonovskydima@users.noreply.github.com>
2026-03-27 01:07:28 +0200

    Refactoring

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "eu-north-1"
+  region = "eu-north-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
@@ -10,15 +10,15 @@ resource "aws_vpc" "core_services" {
 }
 
 resource "aws_subnet" "shared_services" {
-  vpc_id            = aws_vpc.core_services.id
-  cidr_block        = "10.20.10.0/24"
+  vpc_id = aws_vpc.core_services.id
+  cidr_block = "10.20.10.0/24"
   availability_zone = "eu-north-1a"
   tags = { Name = "SharedServicesSubnet" }
 }
 
 resource "aws_subnet" "database" {
-  vpc_id            = aws_vpc.core_services.id
-  cidr_block        = "10.20.20.0/24"
+  vpc_id = aws_vpc.core_services.id
+  cidr_block = "10.20.20.0/24"
   availability_zone = "eu-north-1a"
   tags = { Name = "DatabaseSubnet" }
 }
@@ -29,43 +29,43 @@ resource "aws_vpc" "manufacturing" {
 }
 
 resource "aws_subnet" "sensor1" {
-  vpc_id            = aws_vpc.manufacturing.id
-  cidr_block        = "10.30.20.0/24"
+  vpc_id = aws_vpc.manufacturing.id
+  cidr_block = "10.30.20.0/24"
   availability_zone = "eu-north-1a"
   tags = { Name = "SensorSubnet1" }
 }
 
 resource "aws_subnet" "sensor2" {
-  vpc_id            = aws_vpc.manufacturing.id
-  cidr_block        = "10.30.21.0/24"
+  vpc_id = aws_vpc.manufacturing.id
+  cidr_block = "10.30.21.0/24"
   availability_zone = "eu-north-1a"
   tags = { Name = "SensorSubnet2" }
 }
 
 resource "aws_security_group" "nsg_secure" {
-  name   = "myNSGSecure"
+  name  = "myNSGSecure"
   vpc_id = aws_vpc.core_services.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "AllowASG HTTP"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "AllowASG HTTPS"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["10.0.0.0/8"]
     description = "DenyInternetOutbound"
   }
@@ -79,9 +79,9 @@ resource "aws_route53_zone" "public" {
 
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.public.zone_id
-  name    = "www.contoso.com"
-  type    = "A"
-  ttl     = 1
+  name = "www.contoso.com"
+  type = "A"
+  ttl = 1
   records = ["10.1.1.4"]
 }
 
@@ -95,8 +95,8 @@ resource "aws_route53_zone" "private" {
 
 resource "aws_route53_record" "sensorvm" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "sensorvm.private.contoso.com"
-  type    = "A"
-  ttl     = 1
+  name = "sensorvm.private.contoso.com"
+  type = "A"
+  ttl = 1
   records = ["10.1.1.4"]
 }
