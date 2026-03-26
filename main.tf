45c8b6484594345945a161815213191cc654b2a7
yablonovskydima <157278934+yablonovskydima@users.noreply.github.com>
2026-03-27 01:07:51 +0200

    Refactoring

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region     = "eu-north-1"
+  region = "eu-north-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
 resource "aws_ebs_volume" "disk1" {
   availability_zone = "eu-north-1a"
-  size              = 32
-  type              = "standard"
+  size = 32
+  type = "standard"
 
   tags = {
     Name = "az104-disk1"
@@ -16,8 +16,8 @@ resource "aws_ebs_volume" "disk1" {
 
 resource "aws_ebs_volume" "disk2" {
   availability_zone = "eu-north-1a"
-  size              = 32
-  type              = "standard"
+  size = 32
+  type = "standard"
 
   tags = {
     Name = "az104-disk2"
